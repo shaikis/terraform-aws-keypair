5f54734aae51056be02e69c518aff37edc2dbcff
Ismails <shaik.urs@gmail.com>
2021-12-07 22:14:10 +0530

    added output

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "key_pair_arn" {
-    value = aws_key_pair.kp.[0].arn
+    value = aws_key_pair.kp.*.arn
 }
 
 output "keypair_arn" {
-    value = aws_key_pair.keypair.[0].arn
+    value = aws_key_pair.keypair.*.arn
 }
