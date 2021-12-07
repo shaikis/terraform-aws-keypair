7dd49f13c6485509e322873a7d5a3f50b44ad5e5
Ismails <shaik.urs@gmail.com>
2021-12-07 22:10:15 +0530

    updated resource name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "pk" {
 
 resource "aws_key_pair" "kp" {
   count      = var.pub_key != null ? 0 : 1 
-  key_name   = var.key_name      # Create a "myKey" to AWS!!
+  key_name   = var.key_name[count.index]      # Create a "myKey" to AWS!!
   public_key = tls_private_key.pk.public_key_openssh
 
   provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
@@ -15,6 +15,6 @@ resource "aws_key_pair" "kp" {
 
 resource "aws_key_pair" "keypair" {
   count = var.pub_key != null ? 1 : 0
-  key_name   = var.key_name     # Create a "myKey" to AWS!!
+  key_name   = var.key_name[count.index]     # Create a "myKey" to AWS!!
   public_key = var.pub_key
 }
