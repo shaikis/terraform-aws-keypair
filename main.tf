6d8585477f022026b7f55353fc0b007ebaaee622
Ismails <shaik.urs@gmail.com>
2021-12-08 20:46:31 +0530

    modified keypair

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ resource "tls_private_key" "pk" {
 
 resource "aws_key_pair" "kp" {
   count      = var.pub_key != null ? 0 : 1 
-  key_name   = var.key_name[count.index]      # Create a "myKey" to AWS!!
+  key_name   = var.key_name      # Create a "myKey" to AWS!!
   public_key = tls_private_key.pk.public_key_openssh
 }
 
 resource "aws_key_pair" "keypair" {
   count = var.pub_key != null ? 1 : 0
-  key_name   = var.key_name[count.index]     # Create a "myKey" to AWS!!
+  key_name   = var.key_name    # Create a "myKey" to AWS!!
   public_key = var.pub_key
 }
