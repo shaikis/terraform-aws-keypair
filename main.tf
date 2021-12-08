faa16d90d26aeef6a107d5853b3a3e88181dab84
Ismails <shaik.urs@gmail.com>
2021-12-08 21:24:58 +0530

    added output

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "tls_private_key" "pk" {
 resource "aws_key_pair" "kp" {
   count      = var.pub_key != null ? 0 : 1 
   key_name   = var.key_name      # Create a "myKey" to AWS!!
-  public_key = tls_private_key.pk.public_key_openssh
+  public_key = tls_private_key.pk.*.public_key_openssh
 }
 
 resource "aws_key_pair" "keypair" {
