ff5dfc9d25db704f621b1af8ce9d64deec3e5c0a
Ismails <shaik.urs@gmail.com>
2021-12-09 11:18:57 +0530

    added some changes

--- output.tf
+++ output.tf
@@ -1,15 +1,9 @@
 output "key_pair_arn" {
     value = aws_key_pair.kp.*.arn
 }
-output "key_pair_name" {
-    value = aws_key_pair.kp.*.name
-}
+
 output "keypair_arn" {
     value = aws_key_pair.keypair.*.arn
-}
-
-output "keypair_name" {
-    value = aws_key_pair.keypair.*.name
 }
 
 output "private_key" {
